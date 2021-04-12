88acf6398696b9693da61cf274858235339a7b61
Lufuno <ltshishivhiri@gmail.com>
2021-04-12 22:09:42 +0200

    getting list of  users online

--- server/Server.java
+++ server/Server.java
@@ -8,9 +8,6 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-import data.Connection;
-import data.Packet;
-import data.PacketHandler;
 import utils.*;
 
 //Server class for handling a multi- threaded inistance of a UDP server
@@ -45,103 +42,7 @@ public class Server
 	}
 
 
-// 	//Server initialisation
-// 	//@throws SocketException
-// 	//
-// 	/*public void init() throws SocketException
-// 	{
-// 		this.socket = new DatagramSocket(this.port);
-// 		process = new Thread(this, "server_process");
-// 		process.start();
-// 	}
-// 	*/
-// 	//get port the server is binded to
-// 	//@return port
-// 	//
-// 	public int getPort()
-// 	{
-// 		return port;
-// 	}
 
-// 	//Send a packet to client
-// 	//
-// 	public void send(final Packet packet)
-// 	{
-// 		//send = new Thread("send_thread"){
-// 		//	public void run(){
-// 				DatagramPacket dgpack = new DatagramPacket(
-// 						packet.getData(),
-// 						packet.getData().length,
-// 						packet.getAddr(),
-// 						packet.getPort()
-// 				);
-
-// 				try{
-// 					socket.send(dgpack);
-
-// 				}catch(IOException e)
-// 				{
-// 					System.out.println(e.getMessage());
-// 				}
-// 		//	}
-// 	//	};
-// 	//	send.start();
-// 	}
-
-// 	//broadcast packet to all connected users
-// 	//@param packet
-// 	//
-// 	public void broadcast(byte[] data)
-// 	{
-// 		for(Connection c : CLIENTS)
-// 		{
-// 			send(new Packet(data, c.getAddress(), c.getPort()));
-// 		}
-// 	}
-
-// 	//receiving packet from clients
-// 	//
-// 	public void receive()
-// 	{
-// 	//	receive = new Thread("receive_thread")
-// 	//	{
-// 	//		public void run(){
-// 				while(running)
-// 				{
-					
-					// try {
-					// 	byte[] buffer = new byte[1024];
-					// 	DatagramPacket dgpacket = new DatagramPacket(buffer, buffer.length);
-					// 	socket.receive(dgpacket);
-					// 	Packet recievedPacket = new Packet(dgpacket.getData(), dgpacket.getAddress(), dgpacket.getPort());
-
-					// 	if(dgpacket.getData().length > 5){
-					// 		this.send(recievedPacket);
-					// 	}
-
-					// } catch (IOException e) {
-						
-					// 	e.printStackTrace();
-					
-					// }					
-
-// 					//handler.process(new Packet(dgpacket.getData(), dgpacket.getAddress(), dgpacket.getPort()));
-
-// 				}
-// 		//	}
-// 	//	};
-
-// 	//	receive.start();
-// 	}
-
-// 	//run method for runnable thread
-// 	//
-// 	/*public void run()
-// 	{
-// 		running = true;
-// 		System.out.println("Server started on port " + port);
-// 		this.receive();
-// 	}*/
 	public void listen(){
 		System.out.println("Server is listening on port: "+this.port);
 		while(running){	
@@ -157,16 +58,18 @@ public class Server
 						send(new Message(request.getUser(), true,"registered" , request.getUser().getId()));
 					}
 					else if(request.getText().equals(":q")){
-						send(new Message(request.getUser(), true,":q" , request.getUser().getId()));
 						CLIENTS.remove(request.getUser());
+						send(new Message(request.getUser(), true,":q" , request.getUser().getId()));
+						
 					}
 					else if(request.getText().equals(":l")){
 						send(new Message(request.getUser(), true, getAvailableUsers(), request.getUser().getId()));
 					}
 				}
-				else
+				else{
+					distributeMessage(request);
+				}
 				
-				distributeMessage(request);
 
 			} catch (IOException e) {
 				
@@ -197,7 +100,7 @@ public class Server
 
 	public void distributeMessage(Message message){
 		String destination = message.getDestination();
-		if(!destination.equals("server")){
+		if(destination.equals("server")){
 			System.out.println("non server branch");
 			User destUser = searchClientById(destination);
 			if(destUser != null){
